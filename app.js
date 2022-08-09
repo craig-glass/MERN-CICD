b31fbe6c3f3b36b7c62df1882b01af0359108e60
craig-glass <sydrther@gmail.com>
2022-08-09 15:32:06 +0100

    Update

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, '/frontend/build')));
 
 app.get('/names', (req, res) => {
-    res.send("Craig Glassius");
+    res.send("Craig Glass");
 })
 
 app.listen(5000, () => {
